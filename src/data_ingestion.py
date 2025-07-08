18d3d53f4e7824d56bce327b62f531607259c201
Nite2005 <kumarnitesh140905@gmail.com>
2025-07-08 23:12:39 +0530

    3rd exp

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -5,5 +5,7 @@ import os
 df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/refs/heads/master/Ecommerce%20Customers.csv')
 
 
+df = df.iloc[:,:4]
+
 
 df.to_csv(os.path.join('data','customer.csv'))
