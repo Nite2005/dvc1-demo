db31e109ee76dd8368e0a146098723b2fc474135
Nite2005 <kumarnitesh140905@gmail.com>
2025-07-08 22:55:59 +0530

    exp2

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,8 +4,6 @@ import numpy as np
 import os
 df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/refs/heads/master/Ecommerce%20Customers.csv')
 
-df.drop(columns=['Length of Membership'],inplace=True)
 
-df.iloc[:,3:]
 
 df.to_csv(os.path.join('data','customer.csv'))
